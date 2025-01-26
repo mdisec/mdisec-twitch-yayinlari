34046b55607862348a2b7f58946dc4ee43fbd03e
Arastaci <arastaci@Esat.>
2025-01-26 18:38:42 +0300

    feat(Network-Hacker): add dhcp packet sending tools

--- docs/Network-Hacker/tools/send_arp_packet.py
+++ docs/Network-Hacker/tools/send_arp_packet.py
@@ -1,4 +1,4 @@
-import scapy.all from * 
+from scapy.all import * 
 
 ether_header = Ether(src="00:11:22:33:66:99", dst="ff:ff:ff:ff:ff:ff")
 arp_header = ARP(hwsrc="00:11:22:33:66:99", psrc="10.0.40.67", hwdst="00:00:00:00:00:00", pdst="10.0.40.3", op=1)
